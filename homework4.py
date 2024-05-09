0faecf6644d04f3d691151ca5d3c64f9423f3c88
OlegSud <168107881+OlegSud@users.noreply.github.com>
2024-05-09 16:29:47 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+immutable_var = (1, True, 3, 's')
+print(immutable_var)
+print(type(immutable_var))
+#immutable_var_[1] = 2
+#print(immutable_var)
+#print("Кортеж - это неизменяемая упорядоченная коллекция")
+mutable_list = [1, True, 3, 'z']
+mutable_list[1] = 2
+print(mutable_list)
